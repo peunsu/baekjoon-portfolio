3a5e6e5266dd565be553c4d290b7368a69b36776
peunsu <44690893+peunsu@users.noreply.github.com>
2022-09-04 20:04:23 +0900

    [Silver IV] Title: 큐, Time: 108 ms, Memory: 32460 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,32 @@
+import sys
+from collections import deque
+
+deq = deque()
+
+n = int(input())
+for _ in range(n):
+    cmd = sys.stdin.readline().split()
+    if cmd[0] == "push":
+        deq.append(int(cmd[1]))
+    elif cmd[0] == "pop":
+        if deq:
+            print(deq.popleft())
+        else:
+            print(-1)
+    elif cmd[0] == "size":
+        print(len(deq))
+    elif cmd[0] == "empty":
+        if not deq:
+            print(1)
+        else:
+            print(0)
+    elif cmd[0] == "front":
+        if deq:
+            print(deq[0])
+        else:
+            print(-1)
+    elif cmd[0] == "back":
+        if deq:
+            print(deq[-1])
+        else:
+            print(-1)
