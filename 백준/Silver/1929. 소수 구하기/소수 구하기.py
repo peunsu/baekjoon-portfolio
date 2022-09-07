ab87adb6537ec8de43c67b9135e025df3dfe94be
peunsu <44690893+peunsu@users.noreply.github.com>
2022-09-08 01:11:15 +0900

    [Silver III] Title: 소수 구하기, Time: 228 ms, Memory: 38512 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,15 +1,16 @@
-def check_prime(n: int):   
-    if n == 1:
-        return False
+def check_prime(m: int, n: int):
+    li = [True] * (n + 1)
+    li[0:2] = [False, False]
        
     for i in range(2, int(n**0.5)+1):
-        if n % i == 0:
-            return False
+        if li[i]:
+            for j in range(i*2, n+1, i):
+                li[j] = False
     
-    return True
+    for i in range(m, n+1):
+        if li[i]:
+            print(i)
         
 
 m, n = map(int, input().split())
-for i in range(m, n+1):
-    if check_prime(i):
-        print(i)
+check_prime(m, n)
