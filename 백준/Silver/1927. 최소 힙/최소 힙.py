c1dd91dba232ef2b86b98ad067a63fcb52cb1443
peunsu <44690893+peunsu@users.noreply.github.com>
2022-09-20 23:27:44 +0900

    [Silver II] Title: 최소 힙, Time: 148 ms, Memory: 36764 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.py
+++ 백준/Silver/1927. 최소 힙/최소 힙.py
@@ -1,33 +1,17 @@
-import sys
+import sys, heapq
 
 input = sys.stdin.readline
 
 n = int(input())
-arr = [0]
+arr = []
 
 for _ in range(n):
     x = int(input())
 
     if x:
-        arr.append(x)
-        cur = len(arr) - 1
-        while cur > 1 and arr[cur] < arr[cur//2]:
-            arr[cur], arr[cur//2] = arr[cur//2], arr[cur]
-            cur //= 2
+        heapq.heappush(arr, x)
     else:
-        if len(arr) == 1:
+        if arr:
+            print(heapq.heappop(arr))
+        else:
             print(0)
-            continue
-
-        arr[-1], arr[1] = arr[1], arr[-1]
-        print(arr.pop())
-        
-        cur = 1
-        while True:
-            next = cur * 2
-            if next + 1 < len(arr) and arr[next + 1] < arr[next]:
-                next += 1
-            if next >= len(arr) or arr[next] >= arr[cur]:
-                break
-            arr[next], arr[cur] = arr[cur], arr[next]
-            cur = next
