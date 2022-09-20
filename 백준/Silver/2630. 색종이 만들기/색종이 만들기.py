5f6b4d653c15384289f99c1f2dbf66bb5e40646d
peunsu <44690893+peunsu@users.noreply.github.com>
2022-09-20 22:09:38 +0900

    [Silver II] Title: 색종이 만들기, Time: 88 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.py
@@ -1,39 +1,31 @@
 import sys
-from math import log2
 
 input = sys.stdin.readline
 
+cnt_w, cnt_b = 0, 0
+
+def paper(x: int, y: int, n: int):
+    global cnt_w, cnt_b
+
+    start = arr[x][y]
+    for i in range(x, x + n):
+        for j in range(y, y + n):
+            if arr[i][j] != start:
+                paper(x, y, n//2)
+                paper(x+n//2, y, n//2)
+                paper(x, y+n//2, n//2)
+                paper(x+n//2, y+n//2, n//2)
+                return
+    
+    if start:
+        cnt_b += 1
+    else:
+        cnt_w += 1
+
 n = int(input())
 arr = [list(map(int, input().split())) for _ in range(n)]
-visited = [[0] * n for _ in range(n)]
 
-cnt_w = 0
-cnt_b = 0
-
-nl = int(log2(n))
-for k in range(nl + 1):
-    for l in range(2**k):
-        for m in range(2**k):
-            sum_w = 0
-            sum_b = 0
-            for i in range(l * (2**(nl-k)), (l + 1) * (2**(nl-k))):
-                for j in range(m * (2**(nl-k)), (m + 1) * (2**(nl-k))):
-                    if visited[i][j]:
-                        break
-                    if arr[i][j] == 1:
-                        sum_b += 1
-                    else:
-                        sum_w += 1
-            if sum_w == 4**(nl-k):
-                for i in range(l * (2**(nl-k)), (l + 1) * (2**(nl-k))):
-                    for j in range(m * (2**(nl-k)), (m + 1) * (2**(nl-k))):
-                        visited[i][j] = 1
-                cnt_w += 1
-            elif sum_b == 4**(nl-k):
-                for i in range(l * (2**(nl-k)), (l + 1) * (2**(nl-k))):
-                    for j in range(m * (2**(nl-k)), (m + 1) * (2**(nl-k))):
-                        visited[i][j] = 1
-                cnt_b += 1
+paper(0, 0, n)
 
 print(cnt_w)
 print(cnt_b)
