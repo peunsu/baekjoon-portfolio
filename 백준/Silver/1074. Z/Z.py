037de01fe0941b4b89183e186ceaadd33fe0c168
peunsu <44690893+peunsu@users.noreply.github.com>
2022-09-22 01:41:04 +0900

    [Silver I] Title: Z, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/1074. Z/Z.py
+++ 백준/Silver/1074. Z/Z.py
@@ -1,20 +1,23 @@
 def visit_z(x: int, y: int, n: int):
     global cnt, r, c
     
+    d = 2**(n-1)
+    dc = 4**(n-1)
+    
     if n:
-        if r < y + 2**(n-1):
-            if c < x + 2**(n-1):
+        if r < y + d:
+            if c < x + d:
                 visit_z(x, y, n-1)
             else:
-                cnt += 4**(n-1)
-                visit_z(x+2**(n-1), y, n-1)
+                cnt += dc
+                visit_z(x+d, y, n-1)
         else:
-            if c < x + 2**(n-1):
-                cnt += 4**(n-1) * 2
-                visit_z(x, y+2**(n-1), n-1)
+            if c < x + d:
+                cnt += dc * 2
+                visit_z(x, y+d, n-1)
             else:
-                cnt += 4**(n-1) * 3
-                visit_z(x+2**(n-1), y+2**(n-1), n-1)
+                cnt += dc * 3
+                visit_z(x+d, y+d, n-1)
     else:
         if (x, y) == (c, r):
             print(cnt)
