6ef007a0ec583006c70c260bf94ab3ded5d9145e
peunsu <44690893+peunsu@users.noreply.github.com>
2022-09-22 23:42:43 +0900

    [Silver I] Title: 숨바꼭질, Time: 176 ms, Memory: 34768 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,18 +1,18 @@
+from collections import deque
+
 n, k = map(int, input().split())
 
 pos = [-1] * 100001
 pos[n] = 0
 
-target = {n: {2*n, n-1, n+1}}
-while pos[k] == -1:
-    after_target = dict()
-    for x in target.keys():
-        for i in target[x]:
-            if i < 0 or i > 100000:
-                continue
-            if pos[i] == -1:
-                pos[i] = pos[x] + 1
-                after_target[i] = {2*i, i-1, i+1}
-    target = after_target
+target = deque([n])
+while target:
+    x = target.popleft()
+    for i in (2*x, x-1, x+1):
+        if i < 0 or i > 100000:
+            continue
+        if pos[i] == -1:
+            pos[i] = pos[x] + 1
+            target.append(i)
 
 print(pos[k])
