ea4942c11d4d5bb3d43301b11cc6cf43da850c75
peunsu <44690893+peunsu@users.noreply.github.com>
2023-01-24 14:05:19 +0900

    [Silver I] Title: 경로 찾기, Time: 80 ms, Memory: 30740 KB -BaekjoonHub

---
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+
+def dfs(p: int, visited: list):
+    for q in range(n):
+        if graph[p][q] and not visited[q]:
+            visited[q] = 1
+            dfs(q, visited)
+    return visited
+
+n = int(input())
+graph = [list(map(int, input().split())) for _ in range(n)]
+result = [[0] * n for _ in range(n)]
+
+for i in range(n):
+    print(*dfs(i, [0] * n))
