20a4dea6f9ab405f9241b1b76b77e88e2a641d69
peunsu <44690893+peunsu@users.noreply.github.com>
2023-01-28 13:09:09 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 60 ms, Memory: 34104 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.py
@@ -6,14 +6,12 @@ input = sys.stdin.readline
 n, m = map(int, input().split())
 graph = [0] * 101
 visited = [0] * 101
-result = [100] * 101
 
 for _ in range(n+m):
     a, b = map(int, input().split())
     graph[a] = b
 
 queue = deque([1])
-result[1] = 0
 while queue:
     v = queue.popleft()
 
@@ -22,12 +20,11 @@ while queue:
 
     for i in range(1, 7):
         u = v + i
-        if u <= 100 and not visited[u]:
+        if u <= 100:
             if graph[u]:
                 u = graph[u]
-            
-            queue.append(u)
-            visited[u] = 1
-            result[u] = min(result[u], result[v] + 1)
+            if not visited[u]:
+                queue.append(u)
+                visited[u] = visited[v] + 1
 
-print(result[100])
+print(visited[100])
