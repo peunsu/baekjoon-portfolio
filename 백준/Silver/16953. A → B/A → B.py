5907a0fea65628f14940a922d0a670f92f1faef8
peunsu <44690893+peunsu@users.noreply.github.com>
2023-01-29 15:11:24 +0900

    [Silver II] Title: A → B, Time: 60 ms, Memory: 34116 KB -BaekjoonHub

---
+++ 백준/Silver/16953. A → B/A → B.py
@@ -0,0 +1,36 @@
+import sys
+from collections import deque
+input = sys.stdin.readline
+
+def double(n: int) -> int:
+    return n * 2
+
+def add_one(n: int) -> int:
+    return n * 10 + 1
+
+def half(n: int) -> int:
+    if n % 2 == 0:
+        return n // 2
+    return 0
+
+def remove_one(n: int) -> int:
+    if n % 10 == 1:
+        return n // 10
+    return 0
+
+def bfs(b: int) -> int:
+    queue = deque([(b, 1)])
+
+    while queue:    
+        v, cnt = queue.popleft()
+        for func in [half, remove_one]:
+            n = func(v)
+            if n != 0:
+                if n == a:
+                    return cnt + 1
+                queue.append((n, cnt+1))
+    return -1
+
+a, b = map(int, input().split())
+
+print(bfs(b))
