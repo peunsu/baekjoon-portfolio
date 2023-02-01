6fc531308ad435e9b46548232cd8562efdb69b9a
peunsu <44690893+peunsu@users.noreply.github.com>
2023-02-01 18:52:37 +0900

    [Silver I] Title: 스티커, Time: 884 ms, Memory: 48636 KB -BaekjoonHub

---
+++ 백준/Silver/9465. 스티커/스티커.py
@@ -0,0 +1,19 @@
+import sys
+input = sys.stdin.readline
+
+t = int(input())
+
+for _ in range(t):
+    n = int(input())
+    sticker = [list(map(int, input().split())) for _ in range(2)]
+    
+    for i in range(1, n):
+        if i == 1:
+            sticker[0][i] += sticker[1][i-1]
+            sticker[1][i] += sticker[0][i-1]
+            continue
+
+        sticker[0][i] += max(sticker[1][i-1], sticker[1][i-2])
+        sticker[1][i] += max(sticker[0][i-1], sticker[0][i-2])
+    
+    print(max(list(zip(*sticker))[n-1]))
