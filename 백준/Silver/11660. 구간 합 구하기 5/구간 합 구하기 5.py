df0d5b8ece64e544b0d943e97fdca18f1b21523e
peunsu <44690893+peunsu@users.noreply.github.com>
2023-02-02 19:23:25 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 704 ms, Memory: 73092 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -1,13 +1,13 @@
 import sys
+from itertools import accumulate
+from operator import add
 input = sys.stdin.readline
 
 n, m = map(int, input().split())
-table = [list(map(int, input().split())) for _ in range(n)]
-acc = [[0] * (n+1) for _ in range(n+1)]
+acc = [[0] * (n+1)]
 
 for i in range(1, n+1):
-    for j in range(1, n+1):
-        acc[i][j] = acc[i][j-1] + acc[i-1][j] - acc[i-1][j-1] + table[i-1][j-1]
+    acc.append(list(map(add, [0] + list(accumulate(map(int, input().split()))), acc[i-1])))
 
 for _ in range(m):
     x1, y1, x2, y2 = map(int, input().split())
