c4bdf9370d8fc9afdbe8a0a18d7000dfb2d2875e
peunsu <44690893+peunsu@users.noreply.github.com>
2023-02-04 14:17:12 +0900

    [Gold V] Title: 이진 검색 트리, Time: 1776 ms, Memory: 33684 KB -BaekjoonHub

---
+++ 백준/Gold/5639. 이진 검색 트리/이진 검색 트리.py
@@ -0,0 +1,27 @@
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+
+def postorder(first, end):
+    if first == end:
+        return
+
+    root = tree_pre[first]
+    mid = end
+
+    for i in range(first, end):
+        if tree_pre[i] > root:
+            mid = i
+            break
+
+    postorder(first+1, mid)
+    postorder(mid, end)
+    print(root)
+
+tree_pre = []
+while True:
+    try:
+        tree_pre.append(int(input()))
+    except:
+        break
+postorder(0, len(tree_pre))
