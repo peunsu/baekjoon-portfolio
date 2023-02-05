db144285c272416b6d07d8f03adf4bf3d3b09c0c
peunsu <44690893+peunsu@users.noreply.github.com>
2023-02-05 15:56:23 +0900

    [Gold IV] Title: 거짓말, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.py
+++ 백준/Gold/1043. 거짓말/거짓말.py
@@ -4,14 +4,12 @@ input = sys.stdin.readline
 n, m = map(int, input().split())
 truth = set(map(int, input().split()[1:]))
 party = [set(map(int, input().split()[1:])) for _ in range(m)]
+know = [0] * m
 
-for _ in range(m):
-    for p in party:
+for i in range(m):
+    for j, p in enumerate(party):
         if p.intersection(truth):
+            know[j] = 1
             truth = p.union(truth)
 
-cnt = 0
-for p in party:
-    if not p.intersection(truth):
-        cnt += 1
-print(cnt)
+print(know.count(0))
