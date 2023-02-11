6213360db09456da28fcda0e18974563b298f20b
peunsu <44690893+peunsu@users.noreply.github.com>
2023-02-11 16:35:48 +0900

    [Gold IV] Title: 문자열 폭발, Time: 412 ms, Memory: 42436 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -1,12 +1,13 @@
 string = input()
 explosion = input()
 
+last_explosion = explosion[-1]
 len_explosion = len(explosion)
 
 stack = []
 for s in string:
     stack.append(s)
-    if "".join(stack[-len_explosion:]) == explosion:
+    if stack[-1] == explosion[-1] and "".join(stack[-len_explosion:]) == explosion:
         for _ in range(len_explosion):
             stack.pop()
 
