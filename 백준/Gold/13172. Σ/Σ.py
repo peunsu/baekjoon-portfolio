1ed9dad7388c9afd4564b2bd962c7d48b996e3fd
peunsu <44690893+peunsu@users.noreply.github.com>
2023-02-18 10:17:04 +0900

    [Gold IV] Title: Σ, Time: 64 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Gold/13172. Σ/Σ.py
+++ 백준/Gold/13172. Σ/Σ.py
@@ -2,7 +2,6 @@ import sys
 input = sys.stdin.readline
 X = 1000000007
 
-
 m = int(input())
 result = 0
 
@@ -10,6 +9,6 @@ for _ in range(m):
     a, b = map(int, input().split())
     a = pow(a, -1, X)
     result += (a * b)
-    result %= X
 
+result %= X
 print(result)
