87eceb62caa5f25dc2c08ac590885b7e1d0d69fc
peunsu <44690893+peunsu@users.noreply.github.com>
2023-02-21 20:07:31 +0900

    [Gold II] Title: 피보나치 수 6, Time: 44 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Gold/11444. 피보나치 수 6/피보나치 수 6.py
@@ -0,0 +1,24 @@
+import sys
+input = sys.stdin.readline
+sys.setrecursionlimit(10**6)
+
+def matmul(A, B):
+    return [[sum(a * b for a, b in zip(row_A, col_B)) % 1_000_000_007 for col_B in zip(*B)] for row_A in A]
+
+def matsquare(A, n):
+    if n == 1:
+        return A
+    if n % 2 == 0:
+        temp = matsquare(A, n//2)
+        return matmul(temp, temp)
+    else:
+        temp = matsquare(A, n-1)
+        return matmul(A, temp)
+
+n = int(input())
+fibo = [[1, 1], [1, 0]]
+
+if n == 0:
+    print(0)
+else:
+    print(matsquare(fibo, n)[0][1])
