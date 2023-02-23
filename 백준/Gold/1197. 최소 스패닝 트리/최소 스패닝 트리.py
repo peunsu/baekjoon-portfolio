f72c42bdbca08092aca4e838e3d2e162d9fc40b3
peunsu <44690893+peunsu@users.noreply.github.com>
2023-02-23 20:07:05 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 476 ms, Memory: 63804 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.py
@@ -0,0 +1,31 @@
+import sys
+from heapq import heappush, heappop, heapify
+input = sys.stdin.readline
+
+v, e = map(int, input().split())
+graph = [[] * (v+1) for _ in range(v+1)]
+
+for _ in range(e):
+    a, b, c = map(int, input().split())
+    graph[a].append((c, b))
+    graph[b].append((c, a))
+
+u = 1
+sum_c = 0
+visited = [0] * (v+1)
+visited[u] = 1
+heap = graph[u]
+heapify(heap)
+while heap:
+    c, u = heappop(heap)
+
+    if visited[u]:
+        continue
+
+    visited[u] = 1
+    sum_c += c
+    for c_, u_ in graph[u]:
+        if not visited[u_]:
+            heappush(heap, (c_, u_))
+
+print(sum_c)
