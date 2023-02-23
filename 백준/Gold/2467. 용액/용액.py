d7b765b4ea6a8c8c801cb2e1e71be67bbfbce0d1
peunsu <44690893+peunsu@users.noreply.github.com>
2023-02-23 19:16:57 +0900

    [Gold V] Title: 용액, Time: 136 ms, Memory: 44244 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.py
+++ 백준/Gold/2467. 용액/용액.py
@@ -7,20 +7,21 @@ sols = list(map(int, input().split()))
 
 min_feature_value = float('inf')
 min_sols = []
-for sol in sols:
-    idx = bisect_left(sols, -sol)
-    a, b = idx-1, idx
-    for k in a, b:
-        if not 0 <= k < n:
-            continue
-        if sols[k] == sol:
-            continue
-        feature_value = abs(sol + sols[k])
-        if feature_value == 0:
-            min_sols = [sol, sols[k]]
+left = 0
+right = n-1
+
+while left < right:
+    feature_value = sols[left] + sols[right]
+
+    if abs(feature_value) < min_feature_value:
+        min_feature_value = abs(feature_value)
+        min_sols = [sols[left], sols[right]]
+        if min_feature_value == 0:
             break
-        if min_feature_value > feature_value:
-            min_feature_value = feature_value
-            min_sols = [sol, sols[k]]
+    
+    if feature_value < 0:
+        left += 1
+    else:
+        right -= 1
 
 print(*sorted(min_sols))
