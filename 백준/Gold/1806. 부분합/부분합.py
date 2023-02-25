ce8e5c1f0f2da3d07354649174b2f7cd26881c21
peunsu <44690893+peunsu@users.noreply.github.com>
2023-02-25 15:26:48 +0900

    [Gold IV] Title: 부분합, Time: 120 ms, Memory: 42340 KB -BaekjoonHub

---
+++ 백준/Gold/1806. 부분합/부분합.py
@@ -0,0 +1,34 @@
+import sys
+input = sys.stdin.readline
+
+n, s = map(int, input().split())
+seq = list(map(int, input().split()))
+
+left = 0
+right = 0
+length = 1
+min_length = float('inf')
+total = seq[0]
+while left <= right:
+    if total < s:
+        right += 1
+        length += 1
+
+        if right >= n:
+            break
+
+        total += seq[right]
+        if length > min_length:
+            total -= seq[left]
+            left += 1
+            length = min_length
+    else:
+        total -= seq[left]
+        left += 1
+        min_length = length
+        length -= 1
+
+if min_length == float('inf'):
+    print(0)
+else:
+    print(min_length)
