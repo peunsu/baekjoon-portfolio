d7aa2187b46fcf30d36d8e72b491301e7f2c5784
peunsu <44690893+peunsu@users.noreply.github.com>
2023-03-01 17:06:39 +0900

    [Gold III] Title: 두 배열의 합, Time: 3972 ms, Memory: 70860 KB -BaekjoonHub

--- 백준/Gold/2143. 두 배열의 합/두 배열의 합.py
+++ 백준/Gold/2143. 두 배열의 합/두 배열의 합.py
@@ -2,6 +2,14 @@ import sys
 from bisect import bisect_left, bisect_right
 input = sys.stdin.readline
 
+def bisect_search(sorted, comparison) -> int:
+    cnt = 0
+    for a in comparison:
+        right = bisect_right(sorted, t-a)
+        left = bisect_left(sorted, t-a)
+        cnt += right - left
+    return cnt
+
 t = int(input())
 n = int(input())
 A = list(map(int, input().split()))
@@ -11,11 +19,11 @@ B = list(map(int, input().split()))
 sum_A = [sum(A[i:j+1]) for i in range(n) for j in range(i, n)]
 sum_B = [sum(B[i:j+1]) for i in range(m) for j in range(i, m)]
 
-sum_B.sort()
+if n < m:
+    sum_A.sort()
+    ans = bisect_search(sum_A, sum_B)
+else:
+    sum_B.sort()
+    ans = bisect_search(sum_B, sum_A)
 
-cnt = 0
-for a in sum_A:
-    right = bisect_right(sum_B, t-a)
-    left = bisect_left(sum_B, t-a)
-    cnt += right - left
-print(cnt)
+print(ans)
