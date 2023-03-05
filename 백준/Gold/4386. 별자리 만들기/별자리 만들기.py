33c4738109c23c60e727810b95efc7fb417c7c10
peunsu <44690893+peunsu@users.noreply.github.com>
2023-03-05 14:55:14 +0900

    [Gold III] Title: 별자리 만들기, Time: 56 ms, Memory: 33324 KB -BaekjoonHub

---
+++ 백준/Gold/4386. 별자리 만들기/별자리 만들기.py
@@ -0,0 +1,40 @@
+import sys
+from heapq import heappush, heappop, heapify
+input = sys.stdin.readline
+
+n = int(input())
+parents = [i for i in range(n)]
+
+def dist(vec_a, vec_b):
+    return sum((b-a)**2 for a, b in zip(vec_a, vec_b)) ** 0.5
+
+def find(x):
+    if parents[x] == x:
+        return x
+    return find(parents[x])
+
+def union(x, y):
+    x = find(x)
+    y = find(y)
+    if x < y:
+        parents[y] = x
+    else:
+        parents[x] = y
+
+coord = [tuple(map(float, input().split())) for _ in range(n)]
+edges = []
+
+for i in range(n):
+    for j in range(i, n):
+        edges.append([dist(coord[i], coord[j]), i, j])
+
+
+heapify(edges)
+sum_dist = 0
+while edges:
+    d, u, v = heappop(edges)
+    if find(u) != find(v):
+        union(u, v)
+        sum_dist += d
+
+print(round(sum_dist, 2))
